f2df1a24d7f65bfd1ca2bb433b8e03656ddc3e21
Erik van Bilsen <bilsen@planet.nl>
2026-08-17 15:58:46 -0700

    Progress on 2026 update

--- Neslib.Sokol.Api.pas
+++ Neslib.Sokol.Api.pas
@@ -16,6 +16,9 @@ const
   {$ELSEIF Defined(MACOS64) and Defined(CPUX64) and not Defined(IOS)}
   _LIB_SOKOL = 'libsokol_macos_intel.a';
   _PU = '';
+  {$ELSEIF Defined(MACOS64) and Defined(CPUARM) and not Defined(IOS)}
+  _LIB_SOKOL = 'libsokol_macos_arm.a';
+  _PU = '';
   {$ELSEIF Defined(IOS)}
   _LIB_SOKOL = 'libsokol_ios.a';
   _PU = '';
