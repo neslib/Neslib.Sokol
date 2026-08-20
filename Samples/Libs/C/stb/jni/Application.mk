266cc1b9648f9e832981debb56e93d452d5ff8c8
Erik van Bilsen <bilsen@planet.nl>
2026-08-20 11:08:10 -0700

    Major update to recent Sokol version and added macOS ARM support

--- Samples/Libs/C/stb/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-APP_ABI := armeabi-v7a arm64-v8a
-APP_PLATFORM := android-26
-APP_OPTIM := release
-APP_STL := c++_static
-APP_CPPFLAGS := $(APP_CFLAGS) -std=c++17
--- Samples/Libs/C/stb/stb.c
+++
@@ -1,7 +0,0 @@
-#define STB_IMAGE_IMPLEMENTATION
-
-#if defined(_WIN32)
-#define STBIDEF __declspec(dllexport)
-#endif
-
-#include "stb_image.h"
